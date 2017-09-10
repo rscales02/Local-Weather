a0e306e1694d622f76615ab2e452acd89423ee38
rscales02 <rscales02@yahoo.com>
2017-09-10 06:32:54 -0600

    confused

--- scripts.js
+++ scripts.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-	//html5 geolocator
+  var other = "";
+    //html5 geolocator
     function pos (pos) {
         var lat = pos.coords.latitude;
         var long = pos.coords.longitude;
         var coords = lat + ", " + long;
         console.log(coords); //debug catcher
         displayWeather(lat, long);
-        
+
     }
 
      function getLocation() {
-     	navigator.geolocation.getCurrentPosition(pos);
-     	return false;
+         navigator.geolocation.getCurrentPosition(pos);
+         return false;
      }
 
      var now = new Date();
@@ -19,36 +20,36 @@ $(document).ready(function() {
      $('#time').html(now);
 
      $('#convert').on('click', function(event) {
-     	/* Act on the event */
-     	displayWeather();
-     	if ($('#temperature').hasClass('celcius')) {
-     		temp = f_temp;
-     	} else {
-     		temp = temperature;
-     	} temp = temp.toFixed(2);
-     	$('#temperature').html(temperature);
-     	$('#temperature').toggleClass('celcius farenheit');
+         /* Act on the event */
+         displayWeather();
+         if ($('#temperature').hasClass('celcius')) {
+             other = f_temp;
+         } else {
+             other = temperature;
+         }
+         $('#temperature').html(other);
+         $('#temperature').toggleClass('celcius farenheit');
      });
-         
+
     //weather catcher to return json file
     function displayWeather(lat, long) {
-    	$.ajax({
-    		type: 'GET',
-    		url: 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat + '&lon=' + long,
-    		dataType: 'json',
-    		data: {},
-    		success: function(response) {
-    			whereYouIs = response.name;
-    			$('#whereYouIs').text(whereYouIs); //insert location
-    			temperature = response.main.temp;
-    			f_temp = temperature * 9/5 +32;
-    			$('#temperature').html(temperature); //insert temp
-    			conditions = response.weather[0].description;
-    			$('#conditions').text(conditions); //insert conditions
-    			icon = response.weather[0].icon;
-    			$('#condition_icon').html('<img src=\"' + icon + '\"/>'); //insert conditions icon
-    		}
-    	});
+        $.ajax({
+            type: 'GET',
+            url: 'https://fcc-weather-api.glitch.me/api/current?lat='+ lat + '&lon=' + long,
+            dataType: 'json',
+            data: {},
+            success: function(response) {
+                whereYouIs = response.name;
+                $('#whereYouIs').text(whereYouIs); //insert location
+                temperature = response.main.temp;
+                f_temp = temperature * 9/5 +32;
+                $('#temperature').html(response.main.temp); //insert temp
+                conditions = response.weather[0].description;
+                $('#conditions').text(conditions); //insert conditions
+                icon = response.weather[0].icon;
+                $('#condition_icon').html('<img src=\"' + icon + '\"/>'); //insert conditions icon
+            }
+        });
     }
-	getLocation();
+    getLocation();
 });
